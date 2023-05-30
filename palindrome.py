282a0025edc61ab500ff1c93f0eda2c75ab25bb0
pratik <techvision585@gmail.com>
2023-05-30 11:33:08 +0530

    1st commit

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+def program(word):
+    rword=word[::-1]
+    if rword==word:
+        return True
+    else:
+        return False
+word=input("enter the word you wanted to test: ")
+
+if program(word):
+ print("word is palindrom")
+else:
+ print("word is not palindrom")
---
+++ pythonProject/swap first and last element.py
@@ -0,0 +1,7 @@
+# mylist=[12,35,9,56,24]
+# size=len(mylist)
+# temp=mylist[0]
+# mylist[0]=mylist[size-1]
+# mylist[size-1]=temp
+# print("list after swapping:",mylist)
+
